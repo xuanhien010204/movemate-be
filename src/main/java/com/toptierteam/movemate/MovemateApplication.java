2fd470f4308376bced5baf984ff6cfe5f07d20b2
BuiXuanHien <221967310+xuanhien010204@users.noreply.github.com>
2025-10-08 18:47:20 +0700

    Initial commit

---
+++ src/main/java/com/toptierteam/movemate/MovemateApplication.java
@@ -0,0 +1,13 @@
+package com.toptierteam.movemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toptierteam/movemate/MovemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toptierteam.movemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
